30fbe0748cd3602efbf0c80811be7c3ee8edd39f
Duong Tam Kien <tk@deveha.com>
2011-08-24 21:48:13 +0200

    -lorem

--- api.py
+++ api.py
@@ -173,13 +173,15 @@ def meta():
 	# GROUP BY pos_y
 	pass
 
-@app.route("/data/article/<article_id>")
+@app.route("/data/article/<int:article_id>")
 def get_article(article_id):
 	resp = {}
+
+	a = query_db('SELECT * FROM articles WHERE id=%i' % article_id)[0]
 	
-	resp["title"] = "lorem ipsum"
-	resp["source"] = "lorem ipsum"
-	resp["content"] = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment."
+	resp["title"] = a["title"]
+	resp["source"] =  a["source"]
+	resp["content"] =  a["abstract"]
 
 	return json.dumps(resp)
 
